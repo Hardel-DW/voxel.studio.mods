4432cc33b214102ad6444101bd99961543d91489
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2026-04-13 21:52:00 +0200

    Ok Perfect That work Thanks github i set to false now.

--- src/main/java/fr/hardel/asset_editor/DevFlags.java
+++ src/main/java/fr/hardel/asset_editor/DevFlags.java
@@ -3,7 +3,7 @@ package fr.hardel.asset_editor;
 public final class DevFlags {
 
     public static final boolean DISABLE_SINGLEPLAYER_ADMIN = false;
-    public static final boolean CLEAR_COMPOSE_CACHE = true;
+    public static final boolean CLEAR_COMPOSE_CACHE = false;
 
     private DevFlags() {}
 }
